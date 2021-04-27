a7e14b52eadf9f9f2927849cf1575f831e1d126a
haiphie <82054724+haiphie@users.noreply.github.com>
2021-04-27 20:42:35 +0530

    Theme integrated - 27042021

---
+++ tenacious-pets/assets/custom.js
@@ -0,0 +1,98 @@
+ $(document).ready(function () {
+    $("#ba-slider").twentytwenty({
+      no_overlay: true,
+    });
+  });
+
+$(".social-carousel").flickity({
+    cellAlign: "left",
+    contain: true,
+    initialIndex: 0,
+    wrapAround: true,
+    setGallerySize: false,
+  });
+
+$(window).scroll(function() {    
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 200) {
+        $("#header, #mobile-header").addClass("is-sticky");
+    } else {
+        $("#header, #mobile-header").removeClass("is-sticky");
+    }
+});
+
+
+$('.backtotop  a, .scrllTp').on("click",function(){
+     $('html, body').animate({scrollTop:0}, 'slow');
+        return false;
+});
+
+
+$(".cat-option").click(function () {
+var thisId = $(this).attr("id");
+var cats = $(this).data("cats");
+console.log("CATS: " + cats);
+$(".cat-option").each(function () {
+  if ($(this).attr("id") !== thisId) {
+    $(this).removeClass("selected");
+  }
+});
+$(this).addClass("selected");
+});
+
+
+$('.mobile-menu__toggle-button').on('click', function () {
+	$('.mobile-menu__toggle-icon').toggleClass('is-active');
+});
+
+
+
+ $("document").ready(function () {
+    $(".thumbnail-carousel").flickity({
+      adaptiveHeight: false,
+      cellAlign: "left",
+      contain: true,
+      initialIndex: 0,
+      wrapAround: true,
+      setGallerySize: false,
+    });
+  });
+
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
+
